ca8e631a82d7716fffbabb388f5788bd8ef0a1e9
Joshua Alex <vedanayagamjoshua@gmail.com>
2026-05-31 10:25:18 +0530

    feat: add execution timeout for python runner

--- internal/runner/python.go
+++ internal/runner/python.go
@@ -1,17 +1,16 @@
 package runner
 
 import (
+	"context"
 	"os"
 	"os/exec"
+	"time"
 )
 
 type Result struct {
 	Stdout string
 	Stderr string
 }
-
-
-
 
 func RunPython(source string) (Result, error) {
 
@@ -28,7 +27,26 @@ func RunPython(source string) (Result, error) {
 		return Result{}, err
 	}
 
-	out, err := exec.Command("python3", file).CombinedOutput()
+	// out, err := exec.Command("python3", file).CombinedOutput()
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		2*time.Second,
+	)
+	defer cancel()
+
+	cmd := exec.CommandContext(
+		ctx,
+		"python3",
+		file,
+	)
+	out, err := cmd.CombinedOutput()
+
+	if ctx.Err() == context.DeadlineExceeded {
+		return Result{
+			Stdout: "",
+			Stderr: "execution timed out",
+		}, nil
+	}
 
 	return Result{
 		Stdout: string(out),
